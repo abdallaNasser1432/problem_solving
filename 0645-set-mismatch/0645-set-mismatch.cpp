d88dbe131e539ccd20f8ed32d8999053976880ed
abdallaNasser1432 <99478400+abdallaNasser1432@users.noreply.github.com>
2022-10-24 04:15:41 +0200

    Time: 215 ms (5.01%), Space: 32.9 MB (16.47%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
 
 vector<int> findErrorNums(vector<int>& nums) {
-    int missing,dup;
-    int intialSum= accumulate(nums.begin(),nums.end(),0);
-    set<int> s;
-    for(auto &i: nums)
-        s.insert(i);
-    int sumWithoutDup=accumulate(s.begin(), s.end(), 0);
-    dup=intialSum-sumWithoutDup;
-    int n = nums.size();
-    missing = n * (n+1)/2 - sumWithoutDup;
+    int missing=0,dup=0;
+    map<int,int>m;
+    for (int i = 0; i < nums.size(); ++i) {
+        m[i+1];
+        m[nums[i]]++;
+    }
+    for (auto it=m.begin();it!=m.end() ; ++it) {
+        if(it->second==2)dup=it->first;
+        else if (it->second==0)missing=it->first;
+        if (dup!=0 and missing !=0)
+            break;
+    }
     return {dup,missing};
 
 }
