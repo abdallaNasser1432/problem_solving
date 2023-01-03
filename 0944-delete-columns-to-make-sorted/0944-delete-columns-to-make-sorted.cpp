b173793e003adf6e66a731293492a6e1ee141aaa
abdallaNasser1432 <99478400+abdallaNasser1432@users.noreply.github.com>
2023-01-03 10:07:18 +0200

    Time: 1549 ms (5.18%), Space: 495 MB (5.80%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int minDeletionSize(vector<string>& strs) {
-    int count =0; string temp,sorted;
+int minDeletionSize(vector<string>& strs) {
+    int count =0; int Max=-1;
     for (int i = 0; i < strs[0].size() ; ++i) {
-        for (auto ch:strs)
-            temp+=ch[i];
-        sorted = temp;
-        sort(temp.begin(),temp.end());
-        if(sorted != temp)
-            count++;
-        temp="";
+        for (auto ch:strs){
+            Max=max(Max,(int)ch[i]);
+            if(Max != (int)ch[i]){
+                count++;
+                break;
+            }
+        }
+        Max=-1;
     }
     return count;
 }
