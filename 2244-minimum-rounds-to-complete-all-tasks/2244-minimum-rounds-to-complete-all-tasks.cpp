39514aba01beca432a24f8d7f954942330b714a2
abdallaNasser1432 <99478400+abdallaNasser1432@users.noreply.github.com>
2023-01-04 15:08:54 +0200

    Time: 520 ms (40.19%), Space: 104.7 MB (23.86%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -10,12 +10,8 @@ public:
             return -1;
         else if (it.second % 3== 0)
              round+=it.second/3;
-        else if(it.second % 3 == 2 and it.second!=2)
-            round+=(it.second -2)/3 +1;
-        else if(it.second % 3 == 1)
-            round+=(it.second -4)/3 +2;
         else
-            round++;
+            round+=it.second/3 +1;
     }
     return round;
 }
