a209f9b2e0d17aaae8a828064872a1da19922758
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-01-10 06:27:26 +0200

    Time: 109 ms (25.71%), Space: 46.2 MB (24.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,18 +1,8 @@
 public class Solution {
      public int MajorityElement(int[] nums)
         {
-            Hashtable hash = new Hashtable();
-            
-            foreach (var item in nums)
-                if (hash.ContainsKey(item))
-                    hash[item] = (int)hash[item] + 1;
-                else
-                    hash.Add(item, 1);
-
-            foreach (var key in hash.Keys)
-                if ((int)hash[key] > nums.Length / 2)
-                    return (int)key;
-
-            return -1;
+           Array.Sort(nums);
+int n = nums.Length;
+return nums[n / 2];
         }
 }
