d5bf426429e2353784f2debd51ca08fa63a26aa6
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-01-10 06:42:11 +0200

    Time: 108 ms (28.49%), Space: 46.6 MB (8.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,19 +1,18 @@
 public class Solution {
      public int MajorityElement(int[] nums)
-        {
-            int count = 0; int majorty = 0;
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (count == 0)
-                {
-                    count++;
-                    majorty = nums[i];
-                }
-                else if (majorty == nums[i])
-                    count++;
-                else
-                    count--;
-            }
-            return majorty;
-        }
+{
+    IDictionary<int,int> map = new Dictionary<int,int>();
+    foreach(int num in nums)
+    {
+        if (map.ContainsKey(num)) map[num]++;
+        else map.Add(num, 1);
+    }
+        
+    foreach(var num in map)
+    {
+        if (num.Value > nums.Length / 2)
+            return num.Key;
+    }
+    return 0;
 }
+        }
