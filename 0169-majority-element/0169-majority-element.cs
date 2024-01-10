ff8c9559e3488b4d7a379fe9b594b5b2a0a4adb2
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-01-10 06:36:59 +0200

    Time: 109 ms (25.71%), Space: 46.4 MB (14.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,8 +1,19 @@
 public class Solution {
      public int MajorityElement(int[] nums)
         {
-           Array.Sort(nums);
-int n = nums.Length;
-return nums[n / 2];
+            int count = 0; int majorty = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count == 0)
+                {
+                    count++;
+                    majorty = nums[i];
+                }
+                else if (majorty == nums[i])
+                    count++;
+                else
+                    count--;
+            }
+            return majorty;
         }
 }
