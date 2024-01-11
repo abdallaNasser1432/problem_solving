aba4d2f3ac8e00b175ba1d1d79a6410a50985410
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-01-11 09:09:18 +0200

    Time: 203 ms (40.43%), Space: 61.6 MB (83.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.cs
+++ 0055-jump-game/0055-jump-game.cs
@@ -1,17 +1,19 @@
 public class Solution {
-   public bool CanJump(int[] nums)
+    public bool CanJump(int[] nums)
 {
-    int reach = 0;
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (reach < i) return false;
 
-                if (reach < i + nums[i])
-                    reach = i + nums[i];
-                if(reach>=nums.Length-1)
-                    return true;
-            }
-            
+    for (int reach = 0, i = 0; i < nums.Length; i++)
+    {
+        if (reach < i)
+            return false;
+
+        reach = Math.Max(reach, nums[i] + i);
+
+        if (reach >= nums.Length - 1)
             return true;
+    }
+
+    return true;
 }
+
 }
