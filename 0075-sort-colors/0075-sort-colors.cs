d6a9b1751a724f52e0b5fd1f21ace17e933b2e46
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-02-13 07:03:19 +0200

    Time: 95 ms (77.17%), Space: 45.6 MB (10.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cs
+++ 0075-sort-colors/0075-sort-colors.cs
@@ -1,18 +1,31 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        int temp; int min; int minIndex=0;
-        for (int i = 0; i < nums.Length; i++)
-        {
-            min = nums[i]; minIndex =i;
-            for (int j = i; j < nums.Length; j++)
-            {
-                if (min > nums[j])
-                {
-                    min = nums[j]; minIndex = j;
-                } 
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        for (int i = 0; i < nums.Length; i++){
+            if (nums[i] == 0){
+                red++;
             }
-            temp = nums[i];
-            nums[i] = min;
-            nums[minIndex]= temp;
+            else if (nums[i] == 1){
+                white++;
+            }
+            else{
+                blue++;
+            }
         }
-}}
+        for (int i = 0; i < nums.Length; i++){
+            if (red > 0){
+                nums[i] = 0;
+                red--;
+            }
+            else if (white > 0){
+                nums[i] = 1;
+                white--;
+            }
+            else{
+                nums[i] = 2;
+            }
+        }
+    }
+}
