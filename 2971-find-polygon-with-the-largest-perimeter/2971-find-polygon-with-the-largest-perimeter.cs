75f2096eb62f210a96f72007bd773ca4f31b1cd3
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-02-15 15:56:47 +0200

    Time: 348 ms (42.86%), Space: 70.5 MB (15.38%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cs
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cs
@@ -2,13 +2,12 @@ public class Solution {
     public long LargestPerimeter(int[] nums){
         Array.Sort(nums);
         long[] perfixSum = new long[nums.Length];
-        nums.CopyTo(perfixSum, 0);
-        for (int i = 1; i < perfixSum.Length; i++) perfixSum[i] += perfixSum[i - 1];
+        perfixSum[0] = nums[0];
+        for (int i = 1; i < perfixSum.Length; i++) perfixSum[i] =nums[i] + perfixSum[i - 1];
         for (int i = perfixSum.Length - 1; i >= 2; i--)
         {
-            if (perfixSum[i] - perfixSum[i - 1] < perfixSum[i - 1])
+            if (nums[i] < perfixSum[i - 1])
                 return perfixSum[i];
-
         }
         return -1;
 
