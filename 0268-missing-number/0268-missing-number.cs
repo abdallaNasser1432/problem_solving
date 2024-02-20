67ecf346632e09f8b3a1130cfba28a96b1470189
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-02-20 02:32:39 +0200

    Time: 107 ms (35.58%), Space: 45.9 MB (48.60%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int MissingNumber(int[] nums){
+    int sum = 0;
+    for (int i = 0; i < nums.Length; i++) sum += nums[i];
+    return (nums.Length * (nums.Length + 1) / 2) - sum;
+    }
+}
