1624f64e25adf3786bfc7a793247f795bbb88e71
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-02-26 17:52:58 +0200

    Time: 160 ms (44.76%), Space: 63.3 MB (34.03%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cs
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cs
@@ -1,14 +1,3 @@
 public class Solution {
-    public  int[] SortedSquares(int[] nums){
-        int[] result = new int[nums.Length];
-        int low = 0; int high = nums.Length-1;
-        for (int i = high; i >= 0; i--)
-        {
-            if (Math.Abs(nums[low]) > Math.Abs(nums[high]))
-                result[i] = nums[low] * nums[low++];
-            else
-                result[i] = nums[high] * nums[high--];
-        }
-        return result;
-    }
+    public  int[] SortedSquares(int[] nums)=> nums.Select(x => x * x).Order().ToArray();
 }
