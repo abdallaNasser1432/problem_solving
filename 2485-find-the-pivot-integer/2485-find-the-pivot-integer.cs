0796e20ab4ffa86272634c4c322862cb0fda1e53
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-03-13 08:52:06 +0200

    Time: 22 ms (70.37%), Space: 26.8 MB (69.14%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int PivotInteger(int n) {
+        int sumAll = (n * (n + 1)) / 2;
+        for (int i = 1; i <= n; i++)
+        {
+            int sumI= ((i * (i + 1)) / 2);
+            if (sumI== sumAll-sumI+i) return i;
+        }
+        return -1;
+    }
+}
