7604cd2ac2841af0871f9e98baa6a64b9f43ce80
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-04-03 07:32:39 +0200

    Time: 305 ms (55.54%), Space: 42.3 MB (74.03%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool Exist(char[][] board, string word){
+        
+        int m = board.Length, n = board[0].Length;
+        Func<int, int, int, bool> backtrack = null;
+        backtrack=(i, j, k) =>
+        {
+            if (k == word.Length) return true;
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j]!= word[k]) return false;
+            char temp = board[i][j];
+            board[i][j] = '0';
+            var result = backtrack(i + 1, j, k + 1) ||
+                         backtrack(i - 1, j, k + 1) ||
+                         backtrack(i, j + 1, k + 1) ||
+                         backtrack(i, j - 1, k + 1);
+            board[i][j]= temp;
+            return result;
+        };
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (backtrack(i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+}
