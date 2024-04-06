e1ad5de417c73b7e64d3cd95c584565bc0cb4213
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-04-06 07:46:27 +0200

    Time: 75 ms (35.69%), Space: 46.6 MB (88.79%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
@@ -1,30 +1,24 @@
 public class Solution {
         public string MinRemoveToMakeValid(string s)  {
-        Stack<char> stack = new Stack<char>(); int count = 0;
-        foreach (var item in s)
+        StringBuilder newString = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < s.Length; i++)
         {
-            if (item == ')' && count <= 0) continue;
-            else if (item == ')') count--;
-            else if (item == '(') count++;
-            stack.Push(item);
+            if (s[i] == ')' && count <= 0) continue;
+            else if (s[i] == ')') count--;
+            else if (s[i] == '(') count++;
+            newString.Append(s[i]);
         }
-        var array = stack.ToArray();
         if (count > 0)
         {
-            for (int i = 0; count != 0; i++)
+            for (int i = newString.Length-1; count != 0; i--)
             {
-                if (array[i] == '(')
+                if (newString[i] == '(')
                 {
                     count--;
-                    array[i] = '0';
+                    newString.Remove(i, 1);
                 }
             }
-        }
-        StringBuilder newString = new StringBuilder();
-        for (int i =array.Length-1; i >= 0; i--)
-        {
-            if (array[i] == '0') continue;
-            newString.Append(array[i]);
         }
         return newString.ToString();
     }
