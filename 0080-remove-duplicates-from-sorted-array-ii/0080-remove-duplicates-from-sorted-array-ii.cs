63ddc2989a3a6d742556b3de3254d94d861d97ac
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-05-27 20:44:13 +0300

    Time: 108 ms (51.94%), Space: 47.4 MB (44.84%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
@@ -1,19 +1,11 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        int k = 1; bool twice = false;
-        for (int i = 1; i < nums.Length; i++)
+        int i = 0;
+        foreach (var item in nums)
         {
-            if (nums[i] == nums[i - 1] && !twice)
-            {
-                nums[k++] = nums[i];
-                twice = true;
-            }
-            else if (nums[i] != nums[i - 1])
-            {
-                nums[k++] = nums[i];
-                twice = false;
-            }
+            if (i < 2 || item > nums[i - 2])
+                nums[i++] = item;
         }
-        return k;
+        return i;
     }
 }
