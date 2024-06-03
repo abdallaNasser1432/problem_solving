b9878896f9723edd270765f476ace90de9d6156a
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-06-03 17:50:50 +0300

    Time: 51 ms (53.13%), Space: 44.4 MB (6.25%) - LeetHub

---
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int AppendCharacters(string s, string t) {
+        int sIndex = 0, tIndex = 0;
+        while (sIndex < s.Length && tIndex < t.Length)
+        {
+            if (s[sIndex] == t[tIndex]) tIndex++;
+            sIndex++;
+        }
+        return t.Length - tIndex;
+    }
+}
