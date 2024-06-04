1b029be4ac8572525e099c6af4b7891af5b45fee
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-06-04 06:16:24 +0300

    Time: 69 ms (18.32%), Space: 45.6 MB (46.51%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cs
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -1,28 +1,23 @@
 public class Solution {
     public  string IntToRoman(int num){
-        string[]  array = ["V","IX","IV","I","L","XC","XL","X","D","CM","CD","C"];
+        string[]  array = ["V","IX","IV","I","L","XC","XL"
+                           ,"X","D","CM","CD","C","M", "M"
+                           , "M","M"];
         StringBuilder roman = new StringBuilder();
         string digts = num.ToString(); int Count = digts.Length,index;
         for (int i = 0; i < digts.Length; i++)
         {
-            if(Count == 4)
-            {
-                roman.Append('M', digts[i]-'0');
-            }
+            index = 4 * Count--;
+            if (digts[i] - '0' < 4) roman.Append(char.Parse(array[index - 1]), digts[i] - '0');
+            else if (digts[i] - '0' == 4) roman.Append(array[index - 2]);
+            else if (digts[i] - '0' == 9) roman.Append(array[index - 3]);
             else
             {
-                index = 4 * Count;
-                if (digts[i] - '0' < 4) roman.Append(char.Parse(array[index-1]), digts[i] - '0');
-                else if (digts[i] - '0' == 4) roman.Append(array[index - 2]);
-                else if (digts[i] - '0' == 9) roman.Append(array[index - 3]);
-                else
-                {
-                    roman.Append(array[index - 4]);
-                    roman.Append(char.Parse(array[index - 1]), (digts[i] - '0') - 5);
-                }
+                roman.Append(array[index - 4]);
+                roman.Append(char.Parse(array[index - 1]), (digts[i] - '0') - 5);
             }
-            Count--;
         }
+
         return roman.ToString();
     }
 
