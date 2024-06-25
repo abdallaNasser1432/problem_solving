a32d799b051865ba0725e3e073fd368122ec7045
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-06-25 22:23:32 +0300

    Time: 102 ms (57.54%), Space: 47.1 MB (11.62%) - LeetHub

---
+++ 0135-candy/0135-candy.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int Candy(int[] ratings) {
+        
+      int[]candies = new int[ratings.Length];
+      Array.Fill(candies, 1);
+      for (int i = 1; i < ratings.Length; i++)
+      {
+          if (ratings[i] > ratings[i-1])
+              candies[i]=candies[i-1]+1;
+      }
+      for (int i = candies.Length-2; i >=0; i--)
+      {
+          if (ratings[i] > ratings[i + 1])
+              candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+      }
+      return candies.Sum(); 
+        
+    }
+}
