78496af8fa7d0b2b77f9765df30787c1e2f20563
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-07-24 20:47:24 +0300

    Time: 63 ms (58.50%), Space: 43.3 MB (89.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -1,15 +1,14 @@
 public class Solution {
     public bool IsPalindrome(string s) {
-        StringBuilder palindrome = new StringBuilder();
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (char.IsAsciiLetterOrDigit(s[i]))
-                palindrome.Append(char.ToLower(s[i]));
-        }
-        int l = 0, h = palindrome.Length - 1;
+        int l = 0, h = s.Length - 1;
         while (l < h)
         {
-            if(palindrome[l++] != palindrome[h--])return false;
+            if (!char.IsAsciiLetterOrDigit(s[l])) l++;
+            else if (!char.IsAsciiLetterOrDigit(s[h])) h--;
+            else
+            {
+                if (char.ToLower(s[l++]) != char.ToLower(s[h--])) return false;
+            }
         }
         return true;
     }
