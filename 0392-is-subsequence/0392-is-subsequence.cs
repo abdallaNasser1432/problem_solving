d3da56d02711d08a1d73262d15be25c962a976f8
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-07-25 05:13:26 +0300

    Time: 52 ms (73.90%), Space: 39.8 MB (44.84%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cs
+++ 0392-is-subsequence/0392-is-subsequence.cs
@@ -1,18 +1,8 @@
 public class Solution {
     public bool IsSubsequence(string s, string t) {
-        int lt = 0, ls = 0, ht = t.Length - 1, hs = s.Length - 1;
-        while(lt <= ht && ls<=hs)
-        {
-            if(lt==ht && ls != hs)
-            {
-                lt++ ; ht--;
-                continue;
-            }
-            if (t[lt++] == s[ls]) ls++;
-            if (t[ht--] == s[hs]) hs--;
-        }
-        if (ls > hs)
-            return true;
-        return false;
+        int sp = 0; int tp = 0;
+        while (sp < s.Length && tp < t.Length)
+            if (s[sp] == t[tp++]) sp++;
+        return sp == s.Length; 
     }
 }
