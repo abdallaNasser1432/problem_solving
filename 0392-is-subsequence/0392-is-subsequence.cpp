10c5a21a3f00ed6d7f8e394758352017579b29c3
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-07-25 05:08:38 +0300

    Time: 3 ms (47.31%), Space: 7.8 MB (19.93%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sp = 0; int tp = 0;
+        while (sp < s.length() && tp < t.length())
+            if (s[sp] == t[tp++]) sp++;
+        return sp == s.length(); 
+    }
+};
