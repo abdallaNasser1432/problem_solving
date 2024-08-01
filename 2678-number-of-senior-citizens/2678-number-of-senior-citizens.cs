0e7799d3f22d8dee6b5e5eef49e8f5954dcb0874
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-08-01 11:12:29 +0300

    Time: 57 ms (87.50%), Space: 41.4 MB (82.29%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int CountSeniors(string[] details) {
+        int count = 0;
+        for (int i = 0; i < details.Length; i++)
+        {
+            if ((details[i][11] >= 54 && details[i][12] > 48) || details[i][11] > 54) count++;
+        }
+        return count;
+    }
+}
