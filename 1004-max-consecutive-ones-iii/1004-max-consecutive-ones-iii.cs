002fc05e57a5eced4e0d82961039d3c01b658ab6
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-08-01 05:16:45 +0300

    Time: 191 ms (65.30%), Space: 61.7 MB (37.30%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cs
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cs
@@ -1,22 +1,11 @@
 public class Solution {
     public int LongestOnes(int[] nums, int k) {
-        int maxOnes = 0, currentCount = 0, maxFlip, j;
-        for (int i = 0; i < nums.Length; i++)
+        int windowEnd = 0, windowsStart = 0;
+        for (; windowEnd < nums.Length; windowEnd++)
         {
-            maxFlip = k; j = i;
-            while(maxFlip>=0 && j < nums.Length)
-            {
-                if (nums[j] == 1) currentCount++;
-                else
-                {
-                    if (--maxFlip < 0) break;
-                    currentCount++;
-                }
-                j++;
-            }
-            maxOnes = Math.Max(maxOnes, currentCount);
-            currentCount = 0;
+            if (nums[windowEnd] == 0) k--;
+            if (k < 0 && nums[windowsStart++] == 0) k++;
         }
-        return maxOnes;
+        return windowEnd - windowsStart ; 
     }
 }
