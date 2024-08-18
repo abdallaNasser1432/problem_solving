f18482f74e3fc5e7070d35b6615ac6abca3d9617
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-08-18 17:43:03 +0300

    Time: 450 ms (6.10%), Space: 86.8 MB (6.10%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cs
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cs
@@ -1,20 +1,20 @@
 public class Solution {
     public int NthUglyNumber(int n) {
-        HashSet<long> uglyNumbersSet = new HashSet<long>(); 
-        uglyNumbersSet.Add(1L);
+        SortedSet<long> uglyNumbersSet = new SortedSet<long>(); 
+        uglyNumbersSet.Add(1L); 
 
         long currentUgly = 1L;
         for (int i = 0; i < n; i++)
         {
-            currentUgly = uglyNumbersSet.Min();
+            currentUgly = uglyNumbersSet.First();
             uglyNumbersSet.Remove(currentUgly); 
 
-            
+
             uglyNumbersSet.Add(currentUgly * 2);
             uglyNumbersSet.Add(currentUgly * 3);
             uglyNumbersSet.Add(currentUgly * 5);
         }
 
-        return (int)currentUgly;
+        return (int)currentUgly; 
     }
 }
