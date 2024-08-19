ec587dd2ba9c6014c9502ca0ab7b4d4af4056f2d
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-08-19 21:05:40 +0300

    Time: 24 ms (50.00%), Space: 26.6 MB (97.22%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MinSteps(int n) {
+        int ans = 0;
+        int d = 2;
+        while (n > 1) {
+            while (n % d == 0) {
+                ans += d;
+                n /= d;
+            }
+            d++;
+        }
+        return ans;
+    }
+}
