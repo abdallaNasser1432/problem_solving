2277428d6c60d3bf9473be95fb1c169f423d8248
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-09-17 05:55:19 +0300

    Time: 95 ms (51.84%), Space: 46.7 MB (74.16%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cs
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cs
@@ -17,13 +17,13 @@ public class Solution {
         ListNode node1 = head,
                  node2 = node1.next;
         
-        while (node1 != null && node2 != null)
+        while (node2 != null && node2.next != null)
         {
             if (node1 == node2)
                 return true;
             
             node1 = node1.next;
-            node2 = node2.next?.next;
+            node2 = node2.next.next;
         }
         
         return false;
