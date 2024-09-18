9ee036f6ccee28a290d17c3e4a0f6c16cd0f3865
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2024-09-18 12:03:37 +0300

    Time: 48 ms (86.63%), Space: 39.5 MB (12.76%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cs
+++ 0290-word-pattern/0290-word-pattern.cs
@@ -1,17 +1,18 @@
 public class Solution {
     public bool WordPattern(string pattern, string s) {
+        
         var words = s.Split();
-        HashSet<string> strings = new(words);
-        HashSet<char> chars = new(pattern.ToCharArray());
-        if (pattern.Length != words.Length || strings.Count != chars.Count) return false;
+        if (pattern.Length != words.Length) return false;
         Dictionary<char, string> map = new();
         for (int i = 0; i < pattern.Length; i++)
         {
-            if (map.ContainsKey(pattern[i]))
+            if (map.ContainsKey(pattern[i]) && map.ContainsValue(words[i]))
             {
                 if (map[pattern[i]] != words[i])
                     return false;
             }
+            else if (map.ContainsKey(pattern[i]) || map.ContainsValue(words[i]))
+                return false;
             else
                 map[pattern[i]] = words[i];
         }
