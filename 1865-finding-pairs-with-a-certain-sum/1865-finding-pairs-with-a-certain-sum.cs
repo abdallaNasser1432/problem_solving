95bae2e3a3bfbbbea57746e80d1330098ebaf582
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2025-07-06 08:40:55 +0300

    Time: 336 ms (13.33%), Space:  97.4 MB (93.33%) , Difficulty: Medium

--- 1865-finding-pairs-with-a-certain-sum/1865-finding-pairs-with-a-certain-sum.cs
+++ 1865-finding-pairs-with-a-certain-sum/1865-finding-pairs-with-a-certain-sum.cs
@@ -2,24 +2,39 @@ public class FindSumPairs {
 
     private int[] _nums1 = Array.Empty<int>();
     private int[] _nums2 = Array.Empty<int>();
-    private Dictionary<int, int> freq = new Dictionary<int, int>();
+    private Dictionary<int, int> freq2 = new Dictionary<int, int>();
 
     public FindSumPairs(int[] nums1, int[] nums2)
     {
         _nums1 = nums1;
         _nums2 = nums2;
-        foreach (int i in _nums1)
+        foreach (int i in _nums2)
         {
-            if (freq.ContainsKey(i)) freq[i]++;
-            else freq[i] = 1;
+            if (freq2.ContainsKey(i)) freq2[i]++;
+            else freq2[i] = 1;
         }
     }
 
-    public void Add(int index, int val) =>  _nums2[index] += val;
+    public void Add(int index, int val)
+    {
+        int oldVal = _nums2[index];
+        int newVal = oldVal + val;
+
+        
+        _nums2[index] = newVal;
+
+        freq2[oldVal]--;
+        if (freq2[oldVal] == 0)
+            freq2.Remove(oldVal);
+
+        if (!freq2.ContainsKey(newVal))
+            freq2[newVal] = 0;
+        freq2[newVal]++;
+    }
 
 
-    public int Count(int tot) => _nums2.Sum(p=> freq.ContainsKey(tot - p) ? freq[tot-p] : 0);
-    
+    public int Count(int tot) => _nums1.Sum(p => freq2.ContainsKey(tot - p) ? freq2[tot - p] : 0);
+
     }
 
 /**
