a2c01a6648bf2fedb104dfc56d0ac8de2ddd4c72
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2025-12-04 11:27:06 +0200

    Time: 6 ms (100%), Space:  46.5 MB (73.33%) , Difficulty: Medium

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int CountCollisions(string directions) {
+        int n = directions.Length;
+        int l = 0, r = n - 1;
+
+        while (l < n && directions[l] == 'L') {
+            l++;
+        }
+
+        while (r >= l && directions[r] == 'R') {
+            r--;
+        }
+
+        int res = 0;
+        for (int i = l; i <= r; i++) {
+            if (directions[i] != 'S') {
+                res++;
+            }
+        }
+        return res;
+    }
+}
