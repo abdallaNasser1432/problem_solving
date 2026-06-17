c7a4b446226dcaf6de5fb77ea08066a3af9cd1b0
Abdalla Nasser <99478400+abdallaNasser1432@users.noreply.github.com>
2026-06-17 04:11:35 +0300

    Time: 867 ms (14.53%), Space:  0B (100%) , Difficulty: Easy

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,10 +1,15 @@
 /* Write your T-SQL query statement below */
 
--- Write your PostgreSQL query statement below
-SELECT t1.employee_id, t1.name, COUNT(t2.employee_id) AS reports_count,
-    CAST(ROUND(AVG(CAST(t2.age AS FLOAT)), 0) AS INT) AS average_age
-FROM Employees t1
-JOIN Employees t2
-    ON t1.employee_id = t2.reports_to
-GROUP BY t1.employee_id, t1.name
-ORDER BY t1.employee_id;
+
+SELECT
+    m.employee_id,
+    m.name,
+    COUNT(*) AS reports_count,
+    CAST(ROUND(AVG(CAST(e.age AS FLOAT)), 0) AS INT) AS average_age
+FROM Employees e
+INNER JOIN Employees m
+    ON m.employee_id = e.reports_to
+GROUP BY
+    m.employee_id,
+    m.name
+ ORDER BY m.employee_id;   
